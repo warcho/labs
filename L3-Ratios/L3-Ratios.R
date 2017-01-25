76b944fe6e1213b262136cef3c08fa1096f9b036
rosemerryberry <adamsrc@uw.edu>
2017-01-25 03:02:18 -0800

    Removed table creation

--- L3-Ratios/L3-Ratios.R
+++ L3-Ratios/L3-Ratios.R
@@ -3,17 +3,6 @@
 #Install and require package MASS
 
 
-#Using the built in dataset anorexia, aggregate the data to look at:
-#       1. the number of patients who received a treatment (choose one) who responded positively to treatment
-#       2. the number of patients who recieved a treatment who either did not respond or responded negatively to treatment
-#       3. the number of patients who were in the control group who responded positively to treatment
-#       4. the number of patients who were in the control group who did not respond or responded negatively to treatment
-       
-#Such that you end up with a 2 by 2 table
-
-
-
-
 #What kind of analysis appropriate to perform on a study like this?
 
 
@@ -30,7 +19,7 @@
 
 
 
-#Write a function that allows you to build the table based on what kind of treatment you want 
+#Write a function that allows you to perform your analysis based on what kind of treatment you want 
 #that returns the appropriate calculation
 
 
