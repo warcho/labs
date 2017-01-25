0040bd3d169a677aa47d0485073645abee20650b
rosemerryberry <adamsrc@uw.edu>
2017-01-24 21:31:14 -0800

    L3

--- L2-DataViz/Complete.R
+++ L2-DataViz/Complete.R
@@ -5,10 +5,10 @@ library(ggplot2)
 data <- iris
 
 #basic scatter plot
-
+g<-ggplot(data, aes(Sepal.Length, Sepal.Width, color=factor(Species)))+geom_point()
 
 #lets get a title and axis labels in the mix
-
+g<-g+labs(x="Sepal Length", y="Sepal Width", title='Sepal Length vs Sepal Width')
 
 #lets say we want to then color the graph by species of iris
 #color is then specified in aes because it is a function of the dataset
@@ -16,29 +16,32 @@ data <- iris
 
 
 #legend title is ugly
-
+g<- g+theme(legend.title=element_blank())
 
 #and the legend dots are tiny
+g <- g+ guides(color = guide_legend(override.aes = list(size = 4)))
 
 
 #much better
 
 #shall we change the background color?
+g <- g + theme(panel.background = element_rect(fill="grey75"))
 
 
 #hm you know I changed my mind about all the species being on one graph, lets put it on three
-
+g <- g + facet_wrap(~Species, nrow=1)
 
 #lets add a line to those plots!
-
+#g <- g + stat_smooth()
 
 #that isn't quite what we were looking for, lets do a best fit instead
-
+g <- g + stat_smooth(method = "lm", se=FALSE, col="black")
 
 #you can get all sorts of funky themes in the mix
 #https://github.com/jrnold/ggthemes
 library(ggthemes)
-
+g<- g+theme_economist()+scale_colour_economist()
 #NEAT
 
 #plotting
+g
---
+++ L3-Ratios/L3-Ratios.R
@@ -0,0 +1,45 @@
+#Welcome to Lab 3, please answer the following questions
+
+#Install and require package MASS
+
+
+#Using the built in dataset anorexia, aggregate the data to look at:
+#       1. the number of patients who received a treatment (choose one) who responded positively to treatment
+#       2. the number of patients who recieved a treatment who either did not respond or responded negatively to treatment
+#       3. the number of patients who were in the control group who responded positively to treatment
+#       4. the number of patients who were in the control group who did not respond or responded negatively to treatment
+       
+#Such that you end up with a 2 by 2 table
+
+
+
+
+#What kind of analysis appropriate to perform on a study like this?
+
+
+
+
+#Perform the appropriate choice on the dataset
+
+
+
+
+#What does your result tell you?
+
+
+
+
+
+#Write a function that allows you to build the table based on what kind of treatment you want 
+#that returns the appropriate calculation
+
+
+
+
+
+#Which treatment worked better?
+
+
+
+
+
